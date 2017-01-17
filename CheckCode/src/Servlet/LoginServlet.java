4f2fea646b254d47d656786fb4ee6517a5571e4d
LXY-yu <2579007927@qq.com>
2017-01-17 14:41:06 +0800

    验证码

---
+++ CheckCode/src/Servlet/LoginServlet.java
@@ -0,0 +1,41 @@
+package Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String piccode=(String)request.getSession().getAttribute("piccode");
+		String checkcode=request.getParameter("checkcode");
+		checkcode=checkcode.toUpperCase();
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out=response.getWriter();
+		if(checkcode.equals(piccode)){
+			out.println("验证码输入正确。");
+			
+		}else{
+			out.println("验证码输入错误。");
+		}
+		out.flush();
+		out.close();
+		
+	}
+
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+	}
+
+	
+}
